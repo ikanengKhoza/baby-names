cd3fe17024b8dc97031d355c6e4a838a4888d0e2
Ika Khoza <ikaneng.khoza@gmail.com>
2022-04-24 16:44:02 +0100

    Level 3
    First Part Of Level 3 completed

--- src/NameDisplay.js
+++ src/NameDisplay.js
@@ -2,40 +2,59 @@ import React, { useState } from "react";
 import NameData from "./babyNamesData.json";
 
 //put everything in here, filter after sort
+
+const BabyName = ({ nameProp, handleClick }) => {
+  const { id, name, sex } = nameProp;
+  return (
+   
+    <button onClick={() => handleClick(nameProp)}
+      className="btn-background"
+      style={{
+        backgroundColor: sex === "m" ? "#c40f51" : "none",
+      }}
+    >
+      {name}
+    </button>
+    
+  );
+};
+
 const NameDisplay = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [favourites, setFavourites] = useState([]);
+
+  const handleNameClick = (nameObject) => {
+    setFavourites([...favourites, nameObject]);
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
+      <div>
+        <input
+          type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <div>
+          {favourites.map((fav) => (
+            <BabyName nameProp={fav} />
+          ))}{" "}
+          Click Any Name To Add Favourites
+        </div>
+        <hr />
 
-      {NameData.sort((a, b) => (a.name > b.name ? 1 : -1))
-        .filter((nameObject) => {
-          const { name } = nameObject;
-          if (name.toLowerCase().includes(searchInput.toLowerCase())) {
-            return true;
-          }
-          return false;
-        })
-        .map((element) => {
-          return (
-            <button
-              className="btn-background"
-              style={{
-                backgroundColor: element.sex === "m" ? "#c40f51" : "none",
-              }}
-              key={element.id}
-            >
-              {element.name}
-            </button>
-
-           
-          );
-        })}
+        {NameData.sort((a, b) => (a.name > b.name ? 1 : -1))
+          .filter((nameObject) => {
+            const { name } = nameObject;
+            if (name.toLowerCase().includes(searchInput.toLowerCase())) {
+              return true;
+            }
+            return false;
+          })
+          .map((nameObject) => (
+            <BabyName nameProp={nameObject} handleClick={handleNameClick} />
+          ))}
+      </div>
     </div>
   );
 };
